990c0f665b47740863d9aa446063996cc95b6d78
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2021-05-07 15:48:32 -0300

    Arquivos de aula.

--- aula-20210505/prog02.py
+++ aula-20210505/prog02.py
@@ -1,4 +1,4 @@
-from database import cursor
+from database import cursor, db
 
 if __name__ == '__main__':
 
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # No SQLite, definimos um campo como auto incremento usando AUTOINCREMENT
     comando_sqlite = """
         CREATE TABLE IF NOT EXISTS tb_users(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL,
             email TEXT NOT NULL,
             birth_date DATETIME
@@ -31,4 +31,46 @@ if __name__ == '__main__':
         )
     """
     cursor.execute(comando)
+
+    # 1 -> Selecionar | 2 -> Inserir | 3 -> Apagar
+    opcao = None
+
+    opcao = int(input("Digite a opção(1 -> Selecionar | 2 -> Inserir | 3 -> Apagar): "))
+
+    if opcao == 1:
+        comando = "SELECT * FROM tb_users"
+        cursor.execute(comando)
+        response = cursor.fetchall()
+
+        for user in response:
+            print(user)
+
+    elif opcao == 2:
+        print("Insira dos dados do usuário a seguir")
+        username = input("Informe o username: ")
+        email = input("Informe o email: ")
+        birth_date = input("Informe a data de nascimento no formato correto (YYYY-MM-DD): ")
+
+        comando = f"""
+            INSERT INTO tb_users(username, email, birth_date) VALUES(
+                '{username}', '{email}', '{birth_date}'
+            )
+        """
+
+        cursor.execute(comando)
+        db.commit()
+
+        print(f"{cursor.rowcount} linha(s) afetada(s).")
+
+    # Implementar a opção 3 (Delete de usuário)
+    # Deve-se informar via linha de comando o id do usuário a ser apagado
+    elif opcao == 3:
+        id_usuario = int(input("Informe o ID do usuário a ser apagado: "))
+
+        comando = f"DELETE FROM tb_users WHERE id = {id_usuario}"
+
+        cursor.execute(comando)
+        db.commit()
+
+        print(f"{cursor.rowcount} linha(s) afetada(s)")
 
